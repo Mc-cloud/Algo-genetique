767bf59de9ebd49feec0560b272558da08cc567b
Demaille Melkior <melkior.demaille@student-cs.fr>
2026-01-26 12:35:53 +0100

    J'ai créé un dossier fitness pour pouvoir écrire les fonctions d'évaluation

---
+++ fitness.py
@@ -0,0 +1,11 @@
+import numpy as np
+import numpy.typing as npt
+
+def fitness(individu: npt.NDArray, ) -> float
+
+
+
+def dist_df(trajectoire: np.array):
+    """Prend en entrée une trajectoire sous la forme d'une liste d'array 
+    correspondant aux coordonnées des nœuds, garde uniquement les deux premiers
+    et les deux derniers, pour évaluer """
