05a828bc5db81c6205f6acae218b2642e30fe7ad
Demaille Melkior <melkior.demaille@student-cs.fr>
2026-01-26 13:42:49 +0100

    J'ai adapté la fonction de fitness pour qu'elle tienne compte de la distance entre les k  premiers/derniers nœuds (j'ai rajouté à la fin les k premiers nœuds pour voir s'ils se superposent bien quoi)

--- fitness.py
+++ fitness.py
@@ -4,29 +4,34 @@ from dna.RotTable import RotTable
 from dna.Traj3D import Traj3D
 
 
-def dist_df(coords: list):
+def dist_df(coords: list, nbappend = 1):
     """Prend en entrée une trajectoire sous la forme d'une liste d'array 
     correspondant aux coordonnées des nœuds, garde uniquement premier et dernier
     pour évaluer leur distance euclidienne"""
-    start, finish = coords[0], coords[-1]
-    score = np.linalg.norm(finish-start)
-    return score
+    start = coords[:nbappend]
+    end = coords[-nbappend:]
+    distsq = 0
+
+    for i in range(nbappend):
+        distsq+= np.linalg.norm(start[i]-end[i])**2
+
+    return np.sqrt(distsq)
 
 
 
-def fitness(rot_table: RotTable, seq: str, fct_poids = dist_df, appendend = False) -> float :
+def fitness(rot_table: RotTable, seq: str, fct_poids = dist_df, nbappend = 1) -> float :
     """prend en entrée une table de rotations et un code ADN,
     calcule le chemin et en déduit un score."""
     assert seq != ""
     traj = Traj3D()
 
     # Si l'on veut mesurer la distance entre le premier nucléotide et lui-même rajouté en bout de chaîne
-    if appendend: 
-        seq.append(traj[0])
+    for i in range(nbappend) :
+        seq.append(traj[i])
 
     traj.compute(seq, rot_table)
     coords = traj.getTraj()
-    score = fct_poids(coords)
+    score = fct_poids(coords,nbappend)
     return score
 
 
