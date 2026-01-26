7cfb868e925a05ca017c0bf7483fbffd4a84020f
Demaille Melkior <melkior.demaille@student-cs.fr>
2026-01-26 14:13:05 +0100

    Cette fois-ci la fonction fitness devrait être capable de prendre en entrée un nouveau paramètre 'nbcuts' correspondant au nombre de differents endroits où on veut couper le code adn, pour vérifier que la même table donne bien le bon résultat en partant de points du plasmide différents

--- fitness.py
+++ fitness.py
@@ -6,8 +6,9 @@ from dna.Traj3D import Traj3D
 
 def dist_df(coords: list, nbappend = 1):
     """Prend en entrée une trajectoire sous la forme d'une liste d'array 
-    correspondant aux coordonnées des nœuds, garde uniquement premier et dernier
-    pour évaluer leur distance euclidienne"""
+    correspondant aux coordonnées des nœuds, en joiniant les nbappend premiers
+    nœuds à la fin. On compare ensuite la distance euclidienne entre les
+    nœuds initiaux et ceux rajoutés."""
     start = coords[:nbappend]
     end = coords[-nbappend:]
     distsq = 0
@@ -19,20 +20,27 @@ def dist_df(coords: list, nbappend = 1):
 
 
 
-def fitness(rot_table: RotTable, seq: str, fct_poids = dist_df, nbappend = 1) -> float :
+def fitness(rot_table: RotTable, seq: str, fct_poids = dist_df, nbappend = 1, nbcuts = 0) -> float :
     """prend en entrée une table de rotations et un code ADN,
-    calcule le chemin et en déduit un score."""
-    assert seq != ""
-    traj = Traj3D()
+    calcule le chemin et en déduit un score.
+    Edit : prend aussi un nombre de nœuds finaux à rajouter, 
+    et un nombre de coupures à faire."""
+    nbases = len(seq)
+    assert nbases >= nbappend
+    
+    def eval_une_coupure(seq: str, nbappend: int, indcut: int):
+        traj = Traj3D()
+        traj.compute(seq[indcut:]+seq[:indcut+nbappend], rot_table)
+        coords = traj.getTraj()
+        score = fct_poids(coords,nbappend)
+        return score
+    
+    list_coupes = [i*np.floor(nbases/(nbcuts+1)) for i in range(nbcuts+1)]
 
-    # Si l'on veut mesurer la distance entre le premier nucléotide et lui-même rajouté en bout de chaîne
-    for i in range(nbappend) :
-        seq.append(traj[i])
+    score_sq = sum(eval_une_coupure(seq, nbappend, index)**2 for index in list_coupes)
 
-    traj.compute(seq, rot_table)
-    coords = traj.getTraj()
-    score = fct_poids(coords,nbappend)
-    return score
+    return np.sqrt(score_sq)
+
 
 
 
