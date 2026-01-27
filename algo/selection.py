40915566779af64e04495bf55161f4e40076ffab
Demaille Melkior <melkior.demaille@student-cs.fr>
2026-01-27 15:33:19 +0100

    J'ai essayé de permettre un recuit

--- algo/selection.py
+++ algo/selection.py
@@ -66,7 +66,10 @@ def selection_rang_geometrique(list_ind, taux_selec, q=0.3):
     return select
 
 selections_dic = {"elitiste":selection_elitiste,"tournament":selection_tournament,"roulette":selection_roulette,"rang_reel":selection_rang_reel,"rang_geo":selection_rang_geometrique, "roulette_exp" : selection_roulette_exp}
-def selection(list_ind,taux_selec,select_type):
+def selection(list_ind,taux_selec,select_type, n=None):
+    if n:
+        if select_type == "roulette_exp":
+            return selections_dic[select_type](list_ind,taux_selec, temp = max(100,1000-n*10))
     if select_type in selections_dic:
         return selections_dic[select_type](list_ind,taux_selec)
     return selection_elitiste(list_ind,taux_selec)
