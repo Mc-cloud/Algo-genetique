5d211815cc107720dd190a5c983cae9314586b1f
Demaille Melkior <melkior.demaille@student-cs.fr>
2026-01-27 09:20:17 +0100

    Chorrigé selection roulette exp (changement de la température, passage à une loi de type ressort  et correction du nombre de k sellectionnés) et roulette rang réel (correction du nombre de k sellectionnés). Changé RotTable et __main__ pour éviter les problèmes de compatibilité avec nos modifications. Changé un poil nbcuts dans fitness.

--- algo/selection.py
+++ algo/selection.py
@@ -37,9 +37,9 @@ def selection_roulette(list_ind, taux_selec):
 
     return select
 
-def selection_roulette_exp(list_ind, taux_selec, temp = 1):
-    proba = [np.exp(-ind.score/temp) for ind in list_ind]
-    select = random.choices(list_ind, proba, k = int(taux_selec*len(list_ind)//2))
+def selection_roulette_exp(list_ind, taux_selec, temp=100):
+    proba = [np.exp(-(ind.score**2)/temp) for ind in list_ind]
+    select = random.choices(list_ind, proba, k = int(taux_selec*len(list_ind)))
     return select
 
 def selection_rang_reel(list_ind, taux_selec):
@@ -47,7 +47,7 @@ def selection_rang_reel(list_ind, taux_selec):
     
     proba = [i + 1 for i in range(len(list_ind))]
     
-    n_parents = len(list_ind) // 2
+    n_parents = len(list_ind)
     select = random.choices(list_ind, weights=proba, k=int(taux_selec*n_parents))
     
     return select
