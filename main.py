3aaf385dec4e97037a3b597ccf3b2a271a4d764e
Demaille Melkior <melkior.demaille@student-cs.fr>
2026-01-28 08:36:57 +0100

    bon j'ai mon main

--- main.py
+++ main.py
@@ -24,8 +24,11 @@ for selection_type in selections_dic.keys():
 #     # traj_res.compute(base_seq,res.Rot_table)
 #     # traj_res.draw() #'''
 
-res = AlgoGenetique("dna/table.json",base_seq,nb_indiv,nb_generations,taux_selec,"elitiste")
-best, _, _ = res
+res_exp_normal = AlgoGenetique("dna/table.json",base_seq,nb_indiv,nb_generations,taux_selec,"roulette_exp_norm")
+res_recuit_30 = AlgoGenetique("dna/table.json",base_seq,nb_indiv,nb_generations,taux_selec,"roulette_exp", recuit=True)
+best_exp_normal, score_exp_normal, _ = res_exp_normal
+best_recuit30, score_recuit_30, _ = res_recuit_30
+"""
 score = fitness(best[-1].Rot_table,base_seq,nbcuts=0)
 print(" score : ",best[-1].score,"score final : ",score," via type de selection : ","elitiste")
 # traj_res = Traj3D(want_to_plot=True)
@@ -33,3 +36,7 @@ print(" score : ",best[-1].score,"score final : ",score," via type de selection 
 # traj_res.draw()
 plot_with_slider(get_trajectories(best,base_seq))
 save_trajectory_gif(get_trajectories(best,base_seq))
+"""
+plt.plot(score_recuit_30)
+plt.plot(score_exp_normal)
+plt.show()
