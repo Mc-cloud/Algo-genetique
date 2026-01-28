6a9fe85e3d1c0d847f8ba0810ae37d274ffd3007
Clement <clement.rebola@student-cs.fr>
2026-01-28 10:12:33 +0100

    b

--- main.py
+++ main.py
@@ -10,16 +10,17 @@ from simulsmanager import *
 base_seq = ''.join([line.rstrip('\n') for line in open("data/plasmid_8k.fasta")][1:]) #exemple utilisé de dinucléotide
 
 params = {
-    "nb_individus":8,
-    "nb_generations":10,
-    "taux_selec":0.5,
+    "nb_individus":600,
+    "nb_generations":30,
+    "taux_selec":0.3,
     "selection_type":"elitiste",
     "poisson":False,
     "nb_cuts": 1,
-    "nb_append" : 1
+    "nb_append" : 1,
+    "recuit" :False
     }
 
-simul_and_save_results("data_algo/exemple3",base_seq,params)
+simul_and_save_results("data_algo/exemple5",base_seq,params)
 
-load_and_visualise_timeline("data_algo/exemple3",base_seq)
+load_and_visualise_timeline("data_algo/exemple5",base_seq)
 #load_and_save_gif("gifs/exemple2","data_algo/exemple3",base_seq,fps=5)
